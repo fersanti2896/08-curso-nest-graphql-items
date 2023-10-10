51ae3af74d227e7ed970176204431df0c33587bd
Fernando Nicolás <fersanti2896@gmail.com>
2023-10-09 22:04:24 -0600

    Actualizando un item.

--- src/items/items.resolver.ts
+++ src/items/items.resolver.ts
@@ -36,9 +36,10 @@ export class ItemsResolver {
 
   @Mutation(() => Item)
   updateItem(
-    @Args('updateItemInput') updateItemInput: UpdateItemInput
+    @Args('updateItemInput') updateItemInput: UpdateItemInput,
+    @CurrentUser() user: User
   ): Promise<Item> {
-    return this.itemsService.update( updateItemInput.id, updateItemInput );
+    return this.itemsService.update( updateItemInput.id, updateItemInput, user );
   }
 
   @Mutation(() => Item)
